f629a9861bc5c77c1426785cc7364e61c33d6040
Agatha Grig <agathagrigorian@gmail.com>
2017-06-21 20:51:58 +0000

    completed

---
+++ arrayminilab.rb
@@ -0,0 +1,27 @@
+# farmer_tasks=["Milk the cow", "Churn the butter", "Drive the tractor", "Peel the potatoes", "Plant the corn"]
+
+# farmer_tasks.each do|task|
+
+# puts "I'm so busy! I need to #{task}."
+# # end
+
+# some_numbers=[2, 52, 19, 46, 1000]
+
+# some_numbers.each do |number|
+#     puts (number+10)/2
+# end 
+
+# presidents=["George Washington", "John Adams", "Thomas Jefferson", "James Madison", "James Monroe", "John Quincy Adams"]
+# presidents.each do |reverse|
+#     puts ["George Washington", "John Adams", "Thomas Jefferson", "James Madison", "James Monroe", "John Quincy Adams"].reverse
+# end
+
+bottles_of_milk=[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+bottles_of_milk.each do |milk|
+    puts "#{milk} bottles of milk on the wall"
+end
+
+
+
+
+
